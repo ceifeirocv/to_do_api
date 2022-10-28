c688076b484b45ee5411cc52bdb610f93d666ed0
Jair Oliveira <jair.almeida.oliveira@gmail.com>
2022-10-28 18:14:14 -0100

    add getTodoById updateTodo deleteTodo

--- index.js
+++ index.js
@@ -10,37 +10,88 @@ app.use(express.json());
 let todos = [];
 
 
-app.get('/todos', (req, res) => {
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const todo = todos.find((todo) => todo.id === id);
+  if(!todo) {
+    res.status(400).json({"message":"Page not found"});
+  }
+  res.status(200).json(todo);
+})
+
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  todos = todos.filter((todo) => todo.id !== id);
   res.status(200).json(todos);
-});
+})
+app.put('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const { title, description, in_progress} = req.body;
+  const todoIndex = todos.findIndex((todo) =>todo.id === id);
+
+  if(todoIndex === -1) {
+    res.status(400).json({"message":"Page not found"});
+    return
+  }
+  if(title){
+    if (title.length <= 5 || title.length > 50 ){
+      res.status(400).json({"message":"Title must contain 5 to 50 character"});
+      return;
+    }
+    todos[todoIndex].title = title;
+  }
+  if(description){
+    if (description.length <= 20 || description.length > 50 ){
+      res.status(400).json({"message":"Description must contain 20 to 250 character"});
+      return;
+    }
+    todos[todoIndex].description = description;
+  }
+
+  if (in_progress !== undefined){
+    if (typeof in_progress != 'boolean' ){
+      res.status(400).json({"message":"Status must be true or false"});
+      return;
+    }
+    todos[todoIndex].in_progress = in_progress;
+  }
+  res.status(200).json({
+    "message":`To Do: ${id} updated`,
+    "todo" : todos[todoIndex],
+  });
+})
+
 
 app.post('/todos', (req, res) => {
-  const { title, description } = req.body
+  const { title, description } = req.body;
   
   if (!title || title.length <= 5 || title.length > 50 ){
     res.status(400).json({"message":"Title must contain 5 to 50 character"});
     return;
   }   
-  if (!description || description.length <= 5 || description.length > 250){
-    res.status(400).json({"message":"Description must contain 5 to 50 character"});
+  if (!description || description.length <= 20 || description.length > 250){
+    res.status(400).json({"message":"Description must contain 20 to 250 character"});
     return;
   }
   
   const todo = {
     "id": uuidv4(),
     "title": title,
-    "descripton": description,
+    "description": description,
     "in_progress": true
   };
-
+  
   todos.push(todo);
-
+  
   res.status(201).json({
     "message":`To Do: ${title} added to list`,
     "todo" : todo
   });
 });
 
+app.get('/todos', (req, res) => {
+  res.status(200).json(todos);
+});
 
 app.get('/', (req, res) => {
   res.status(200).json({"message":"ToDo API"});
