e64f81a0ccffc550f59e31300c7c894a71285f70
Jair Oliveira <jair.almeida.oliveira@gmail.com>
2022-11-10 11:53:01 -0100

    uptade file names

--- tests/modelTodo.test.js
+++ tests/modelTodo.test.js
@@ -1,14 +1,14 @@
-import Todo from '../models/Todos.js';
+/* eslint-disable no-undef */
+import Todo from '../models/Todo.js';
 import db from '../config/database.js';
 
-
-beforeEach(async() => {
-  await db.query("TRUNCATE TABLE todos RESTART IDENTITY")
+beforeEach(async () => {
+  await db.query('TRUNCATE TABLE todos RESTART IDENTITY');
 });
 
 describe('Space test suite', () => {
   it('My Space test', async () => {
-    const todos =await Todo.getAll();
+    const todos = await Todo.getAll();
     expect(todos).toEqual([]);
   });
 });
